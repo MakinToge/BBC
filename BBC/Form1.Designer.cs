364073d7c5cc84ef9c56a2b84d2c31ae3e72aa2b
MakinToge <adrien.veroone@gmail.com>
2016-01-21 14:49:43 +0100

    emgu working

--- BBC/Form1.Designer.cs
+++ BBC/Form1.Designer.cs
@@ -32,9 +32,9 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -65,32 +65,32 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(637, 431);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
-            // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(13, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(651, 425);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(676, 553);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.imageBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -100,7 +100,7 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
+        private Emgu.CV.UI.ImageBox imageBox1;
     }
 }
--- BBC/Form1.cs
+++ BBC/Form1.cs
@@ -12,6 +12,7 @@ using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
 using Emgu.CV;
+using Emgu.CV.UI;
 using Emgu.Util;
 using Emgu.CV.Structure;
 using Emgu.CV.CvEnum;
@@ -24,11 +25,13 @@ namespace BBC
         {
             InitializeComponent();
         }
-        private FilterInfoCollection webcam;
-        private VideoCaptureDevice cam;
+        //private FilterInfoCollection webcam;
+        //private VideoCaptureDevice cam;
+
+        private CascadeClassifier cascadeClassifier;
 
         private Capture cap;
-        private HaarCascade haar;
+        //private HaarCascade haar;
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -39,42 +42,73 @@ namespace BBC
             this.Text = string.Format("IsRunning: {0} - IsConnected: {1} - IsStandAlone: {2}", PackageHost.IsRunning, PackageHost.IsConnected, PackageHost.IsStandAlone);
             PackageHost.WriteInfo("I'm running !");
 
-            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-            foreach (FilterInfo VideoCaptureDevice in webcam)
-            {
-                comboBox1.Items.Add(VideoCaptureDevice.Name);
-            }
-            comboBox1.SelectedIndex = 0;
 
-            // passing 0 gets zeroth webcam
-            cap = new Capture(0);
-            // adjust path to find your xml
-            //haar = new HaarCascade("..\\..\\..\\..\\lib\\haarcascade_frontalface_alt2.xml");
+
+
+           
+
+
+            //webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            //foreach (FilterInfo VideoCaptureDevice in webcam)
+            //{
+            //    comboBox1.Items.Add(VideoCaptureDevice.Name);
+            //}
+            //comboBox1.SelectedIndex = 0;
+
+            //// passing 0 gets zeroth webcam
+            //cap = new Capture(0);
+            //// adjust path to find your xml
+            //haar = new HaarCascade("..\\..\\..\\..\\lib\\haarcascade_frontalface_default.xml");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            cam = new VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString);
-            cam.NewFrame += cam_NewFrame;
-            cam.Start();
+            //cam = new VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString);
+            //cam.NewFrame += cam_NewFrame;
+            //cam.Start();
         }
 
-        void cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
-        {
-            Bitmap pic = (Bitmap)eventArgs.Frame.Clone();
-            pictureBox1.Image = pic;
-        }
+        //void cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        //{
+        //    Bitmap pic = (Bitmap)eventArgs.Frame.Clone();
+        //    imageBox1.Image = pic;
+        //}
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (cam.IsRunning)
-            {
-                cam.Stop();
-            }
+            //if (cam.IsRunning)
+            //{
+            //    cam.Stop();
+            //}
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            cap = new Capture();
+
+            imageBox1.Image = cap.QueryFrame();
+
+            cascadeClassifier = new CascadeClassifier(Application.StartupPath + "/haarcascade_frontalface_default.xml");
+            using (var imageFrame = cap.QueryFrame().ToImage<Bgr, Byte>())
+            {
+                if (imageFrame != null)
+                {
+                    var grayframe = imageFrame.Convert<Gray, byte>();
+                    var faces = cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, Size.Empty); //the actual face detection happens here
+                    foreach (var face in faces)
+                    {
+                        imageFrame.Draw(face, new Bgr(Color.BurlyWood), 3); //the detected face(s) is highlighted here using a box that is drawn around it/them
+
+                    }
+                }
+                imageBox1.Image = imageFrame;
+            }
+
+
+
+
+
+
             /*using (Image<Bgr, byte> nextFrame = cap.QueryFrame())
             {
                 if (nextFrame != null)
@@ -96,6 +130,11 @@ namespace BBC
                     pictureBox1.Image = nextFrame.ToBitmap();
                 }
             }*/
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
         }
 
         
