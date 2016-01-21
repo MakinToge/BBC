bd3def0111734c05cd40406c0af7dc046fb37573
MakinToge <adrien.veroone@gmail.com>
2016-01-21 15:33:29 +0100

    new clean solution

---
+++ BBCv2/BBCv2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using Constellation;
+using Constellation.Host;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+
+namespace BBCv2
+{
+    public partial class Form1 : Form
+    {
+
+        private Capture cap;
+        private CascadeClassifier cascadeClassifier;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Attach the MessageCallbacks on this class & update the PackageDescriptor
+            PackageHost.AttachMessageCallbacks(this);
+            PackageHost.DeclarePackageDescriptor();
+
+            this.Text = string.Format("IsRunning: {0} - IsConnected: {1} - IsStandAlone: {2}", PackageHost.IsRunning, PackageHost.IsConnected, PackageHost.IsStandAlone);
+            PackageHost.WriteInfo("I'm running !");
+
+            cap = new Capture();
+
+            imageBox1.Image = cap.QueryFrame();
+
+            cascadeClassifier = new CascadeClassifier(Application.StartupPath + "/haarcascade_frontalface_default.xml");
+            using (var imageFrame = cap.QueryFrame().ToImage<Bgr, Byte>())
+            {
+                if (imageFrame != null)
+                {
+                    var grayframe = imageFrame.Convert<Gray, byte>();
+                    var faces = cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, Size.Empty); //the actual face detection happens here
+                    foreach (var face in faces)
+                    {
+                        imageFrame.Draw(face, new Bgr(Color.BurlyWood), 3); //the detected face(s) is highlighted here using a box that is drawn around it/them
+
+                    }
+                }
+                imageBox1.Image = imageFrame;
+            }
+
+        }
+    }
+}
---
+++ BBCv2/BBCv2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Constellation;
+using Constellation.Host;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BBCv2
+{
+    class Program : PackageBase
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args = null)
+        {
+            PackageHost.Start<Program>(args);
+        }
+
+        public override void OnStart()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public override void OnShutdown()
+        {
+            Application.Exit();
+        }
+    }
+}
